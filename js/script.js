73184ae750f6305a40eaa277e916693500e94ea6
abdicore <abdirhamanmohamed@gmail.com>
2024-07-30 21:50:43 +0200

    Descrizione-fasi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log ('js ok')
+// Chiedi all’utente il suo nome,poi chiedi il suo cognome,poi chiedi il suo colore preferito infine stampa in console nomecognomecolorepreferito21
+
+
+/* 
+1-Recuperare gli elementi di interesse nella pagina
+2- Chiedi il nome del utente
+3- Chiedi il cogniome del utente
+4- Chiedi il suo colore preferito
+5- Stampa in console nomecognomecolorepreferito21
+*/ 
+
+
+// Fase preparatoria
+
+
+
+// Fase raccolta dati
+
+
+
+// Fase di lavorazione dati
+
+
+
+// fase produzione imput
